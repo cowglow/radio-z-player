d480a853bf970be57c928cef60e820b2fc65d8c3
Philip Saa <cowglow@gmail.com>
2024-01-22 22:17:44 +0100

    First commit towards player

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,7 @@
 <script>
-  // import svelteLogo from './assets/svelte.svg'
-  // import Counter from './lib/Counter.svelte'
   import App from "./lib/pages/App.svelte";
-  import StreamPlayer from "./lib/StreamPlayer.svelte";
 </script>
 
 <main>
   <App/>
-  <StreamPlayer/>
 </main>
--- src/lib/pages/App.svelte
+++ src/lib/pages/App.svelte
@@ -1,45 +1,32 @@
 <script>
-import svelteLogo from "../../assets/svelte.svg";
-import Counter from "../Counter.svelte";
-
+// import StreamPlayer from "../components/StreamPlayer.svelte";
+import StreamPlayer from "../components/StreamPlayer.svelte";
 </script>
 
-<div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-        <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="player">
+    <div class="head">
+        <div class="back"></div>
+        <div class="front">
+            <div class="avatar">
+                <img src="http://vignette4.wikia.nocookie.net/lyricwiki/images/1/15/Semisonic_portrait.jpg/revision/latest?cb=20090409134209" alt="Artist Avatar">
+            </div>
+            <div class="infos">
+                <div class="titulo_song">
+                    KFC - Killing Fabulous Chickens
+                </div>
+                <div class="duracao_song">
+                    <i class="fa fa-clock-o"></i> Total time 45:12
+                </div>
+                <div class="tags">
+                    <span>Educativo</span>
+                    <span>Galinhas</span>
+                    <span>Podcast</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   <StreamPlayer/>
+
+    <div class="rotation"></div>
 </div>
-<h1>Vite + Svelte</h1>
-
-<div class="card">
-    <Counter />
-</div>
-
-<p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-</p>
-
-<p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-</p>
-
-<style>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-    }
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-    .read-the-docs {
-        color: #888;
-    }
-</style>
