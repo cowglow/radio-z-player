627dd9da405fe5f2cff85a4ac1e5709b31099f01
Philip Saa <cowglow@gmail.com>
2024-01-22 01:03:42 +0100

    SvelteKit Audio player

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  // import svelteLogo from './assets/svelte.svg'
+  // import Counter from './lib/Counter.svelte'
+  import App from "./lib/pages/App.svelte";
+  import StreamPlayer from "./lib/StreamPlayer.svelte";
+</script>
+
+<main>
+  <App/>
+  <StreamPlayer/>
+</main>
