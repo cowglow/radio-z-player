29980e103201a2a5f1db61b8e2fd81234996f30b
Philip Saa <cowglow@gmail.com>
2024-02-03 10:25:02 +0100

    base path updated

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,10 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    // other configuration options
+    paths: {
+      base: '/radio-z-player/',
+    },
+  },
 }
