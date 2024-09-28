4eafaf74bc6b731022845b4a7e972ec64dadd9d4
Philip Saa <cowglow@gmail.com>
2024-09-28 21:18:48 +0200

    Add font and wip loader

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,22 +1,16 @@
 <div class="head">
+    <div class="avatar">
+        <img src="https://radio-z.s3-cdn.welocal.cloud/sources/621_prtdr8vquaxrw_9x1anqvmzkgee.svg"
+             alt="Radio-Z Dot Net Logo">
+    </div>
     <div class="front">
-        <div class="avatar">
-            <img src="https://radio-z.s3-cdn.welocal.cloud/sources/621_prtdr8vquaxrw_9x1anqvmzkgee.svg" alt="Radio-Z Dot Net Logo">
-        </div>
         <div class="infos">
             <div class="title">
-                OSS Radio-Z.net Web Player
+                OSS <a href="https://radio-z.net" target="_blank" rel="noreferrer nofollow">Radio-Z.net</a> Web Player
             </div>
             <div class="description">
                 Dein Community Radio aus Nürnberg
             </div>
-            <!--
-            <div class="actions">
-                <span>vorher</span>
-                <span>aktuell</span>
-                <span>kommt</span>
-            </div>
-            -->
         </div>
     </div>
 </div>
--- src/lib/components/StreamPlayer.svelte
+++ src/lib/components/StreamPlayer.svelte
@@ -1,7 +1,10 @@
 <script>
     import "./StreamPlayer.styles.css"
+    import {onMount} from "svelte";
+    import AsyncProgress from "./AsyncProgress.svelte";
 
     let active = false
+    let loading = false
     const streamUrl = "https://stream.radio-z.net/live"
     const audioFile = new Audio(streamUrl)
 
@@ -11,26 +14,42 @@
 
     function playBtnInit(node) {
         node.addEventListener("click", () => {
-            toggleActive()
+            loading = !loading
             if (active) {
                 audioFile.play()
             } else {
                 audioFile.pause()
             }
+            toggleActive()
         })
     }
+
+    onMount(() => {
+        const onEnded = () => {
+            loading = false
+        }
+        audioFile.addEventListener("loadeddata", onEnded)
+        return () => {
+            audioFile.removeEventListener("loadeddata", onEnded)
+        }
+    })
 </script>
 
 <div class="timeline">
-    <div class="soundline"></div>
     <div class="controllers">
         <button class="play" use:playBtnInit>
             {#if active}
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                    <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/>
-                </svg>
-            {:else}
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/>
+                    </svg>
+                    <div class="soundline"></div>
+                </div>
+            {/if}
+            {#if !active && loading}
+                <AsyncProgress/>
+            {:else }
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
                     <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                     <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>
